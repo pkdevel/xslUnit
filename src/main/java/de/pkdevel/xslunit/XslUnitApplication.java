ad62747c1861b2307c5e99bc1696dc842d2f6e67
Philipp Kolb <kolb@transporeon.com>
2015-03-02 16:01:56 +0100

    logging attempt

--- src/main/java/de/pkdevel/xslunit/XslUnitApplication.java
+++ src/main/java/de/pkdevel/xslunit/XslUnitApplication.java
@@ -10,7 +10,12 @@ import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class XslUnitApplication extends Application {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(XslUnitApplication.class);
 	
 	private Stage primaryStage;
 	
@@ -25,13 +30,16 @@ public final class XslUnitApplication extends Application {
 		final URL fxml = ClassLoader.getSystemResource("META-INF/view/XslUnitGui.fxml");
 		final Parent root = FXMLLoader.load(fxml);
 		final Scene scene = new Scene(root);
-		final Preferences userPrefs = Preferences.userNodeForPackage(this.getClass());
+		
+		final Preferences userPrefs = Preferences.userNodeForPackage(XslUnitApplication.class);
 		final double x = userPrefs.getDouble("stage.x", 100);
 		final double y = userPrefs.getDouble("stage.y", 100);
 		final double width = userPrefs.getDouble("stage.width", 800);
 		final double height = userPrefs.getDouble("stage.height", 600);
+		LOGGER.debug("Found screen prefs: " + x + ":" + y + " " + width + "x" + height);
 		
 		if (Screen.getScreensForRectangle(x, y, width, height).size() != 0) {
+			LOGGER.debug("Screen prefs seem to be legit, taking them into account");
 			this.primaryStage.setX(x);
 			this.primaryStage.setY(y);
 			this.primaryStage.setWidth(width);
@@ -44,11 +52,17 @@ public final class XslUnitApplication extends Application {
 	
 	@Override
 	public void stop() {
-		final Preferences userPrefs = Preferences.userNodeForPackage(this.getClass());
-		userPrefs.putDouble("stage.x", this.primaryStage.getX());
-		userPrefs.putDouble("stage.y", this.primaryStage.getY());
-		userPrefs.putDouble("stage.width", this.primaryStage.getWidth());
-		userPrefs.putDouble("stage.height", this.primaryStage.getHeight());
+		final double x = this.primaryStage.getX();
+		final double y = this.primaryStage.getY();
+		final double width = this.primaryStage.getWidth();
+		final double height = this.primaryStage.getHeight();
+		LOGGER.debug("Saving screen prefs: " + x + ":" + y + " " + width + "x" + height);
+		
+		final Preferences userPrefs = Preferences.userNodeForPackage(XslUnitApplication.class);
+		userPrefs.putDouble("stage.x", x);
+		userPrefs.putDouble("stage.y", y);
+		userPrefs.putDouble("stage.width", width);
+		userPrefs.putDouble("stage.height", height);
 	}
 	
 }
